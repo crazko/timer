aebea996fd5b1f8665803c4a900d2922024185c2
Roman Vesely <crazko@gmail.com>
2017-12-08 22:50:19 +0100

    udpated units size

--- src/Unit.js
+++ src/Unit.js
@@ -7,11 +7,14 @@ export const Unit = ({ handleOnChange, value, isDisabled, title }) => {
   return <div className="flex flex-col-reverse">
     <span className="text-center">{title}</span>
     <input
-      className="bg-white rounded-lg m-2 shadow text-5xl text-right appearance-none border hover:border-grey"
+      className="bg-white rounded-lg m-2 shadow text-right appearance-none border hover:border-grey"
       disabled={isDisabled}
       max="59"
       min="0"
       onChange={handleOnChange}
+      style={{
+        fontSize: '5rem'
+      }}
       type="number"
       value={negative + leadingZero + Math.abs(value)}
     />
